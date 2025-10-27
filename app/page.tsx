010cc50ab64afea7f89d09b090f8ea10e1ce19bb
Alex Linder <sasha.linder@gmail.com>
2025-10-27 12:42:13 +0200

    Clean up Home component by removing console log
    Removed temporary console log for workflow ID.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import App from "./App";
 
-export default function Home() {
-  // Add temporarily in a page/component:
-  console.log("WF", process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID);
+export default function Home() { 
   return <App />;
 }
