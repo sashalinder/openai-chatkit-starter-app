5479747273bae998ab05ad466bb285a2950a196b
Alex Linder <sasha.linder@gmail.com>
2025-10-27 12:35:09 +0200

    Log workflow ID to console in Home component
    Added a console log to display the workflow ID for debugging.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import App from "./App";
 
 export default function Home() {
+  // Add temporarily in a page/component:
+  console.log("WF", process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID);
   return <App />;
 }
