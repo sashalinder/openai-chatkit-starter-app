728ba99afdbe77886ab1139ecd0f667e4f6a83cc
Alex Linder <sasha.linder@gmail.com>
2025-10-27 12:48:30 +0200

    Import DebugEnv and include it in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import App from "./App";
+import DebugEnv from "./DebugEnv";
 
-export default function Home() { 
-  
+export default function Home() {
   return (
     <>
       <DebugEnv />
       <App />
     </>
-    );
+  );
 }
