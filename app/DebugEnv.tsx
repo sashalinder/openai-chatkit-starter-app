be2aa543fb78a2ca523299e51e6f2aeaebc27028
Alex Linder <sasha.linder@gmail.com>
2025-10-27 12:54:32 +0200

    Log OPENAI_API_KEY in DebugEnv component

--- app/DebugEnv.tsx
+++ app/DebugEnv.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export default function DebugEnv() {
+   console.log("OPENAI_API_KEY (client)", process.env.OPENAI_API_KEY);
   console.log("WF (client)", process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID);
   return null;
 }
