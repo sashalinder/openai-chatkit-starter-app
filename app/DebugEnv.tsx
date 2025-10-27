caeed72276149b3e65e936b28d2cca8896fc4a7f
Alex Linder <sasha.linder@gmail.com>
2025-10-27 13:47:00 +0200

    Remove logging of OPENAI_API_KEY

--- app/DebugEnv.tsx
+++ app/DebugEnv.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 export default function DebugEnv() {
-   console.log("OPENAI_API_KEY (client)", process.env.OPENAI_API_KEY);
   console.log("WF (client)", process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID);
   return null;
 }
