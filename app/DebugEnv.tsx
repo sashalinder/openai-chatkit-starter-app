e0c85d670d5801b4e0f31b1d3a5b3ebd71a4a588
Alex Linder <sasha.linder@gmail.com>
2025-10-27 12:50:57 +0200

    Change DebugEnv to default export

--- app/DebugEnv.tsx
+++ app/DebugEnv.tsx
@@ -1,7 +1,6 @@
-// app/DebugEnv.tsx
 "use client";
 
-export function DebugEnv() {
+export default function DebugEnv() {
   console.log("WF (client)", process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID);
-  return null; // renders nothing
+  return null;
 }
